efee887e18aac1d685475a025d46ff120b1ebc24
josema294 <jmaranguren89@gmail.com>
2024-11-01 21:16:47 +0100

    Se calcula dia de semana y horario para determinar 2 tipos de borrado de archivo (working time y no working time)

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import subprocess
+import os
+from datetime import datetime, time
 
 
 def main():
     print("Iniciando proceso")
     downloads_dir = get_dowload_dir()
     scan_dir(downloads_dir)
+
+    if (is_working_time()):
+        delete_working_time()
+    else:
+        delete_No_working()
+
     print("finalizado proceso")
 
 
@@ -15,7 +23,7 @@ def get_dowload_dir():
     downloads = ""
 
     if output.returncode == 0:
-        downloads = output.stdout
+        downloads = output.stdout.strip()
     else:
         downloads = "~/Downloads"
       
@@ -24,7 +32,35 @@ def get_dowload_dir():
 
 
 def scan_dir(downloads_dir):
-    print("scanning")
+    archivos = os.listdir(downloads_dir)
+    rdp_list = []
+    for archivo in archivos:
+        if str.endswith(archivo, ".rdp"):
+            rdp_list.append(archivo)
+
+
+def is_working_time():
+    '''Calculamos si estamos en periodo de trabajo, si estamos trabajando no eliminamos el ultimo archivo rdp
+     ya que podria ser el que necesitemos en el momento'''
+
+    is_working_time = False
+
+    ahora = datetime.now()
+    dia_semana = ahora.weekday()
+    hora = ahora.time()
+
+    if (dia_semana < 5 and (time(8, 0) <= hora <= time(18, 0))):
+        is_working_time = True
+    
+    return is_working_time
+
+
+def delete_working_time():
+    return None
+
+
+def delete_No_working():
+    return None
 
 
 if __name__ == "__main__":
